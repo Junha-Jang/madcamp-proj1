0730ec3b6340ca04b33be7ec7cf85a5db890656b
Junha Jang <andy.jang@kaist.ac.kr>
2023-07-05 00:18:47 +0900

    feat(tab3): fix screen3 directory

--- app/src/main/java/com/example/madcamp_proj1/Tab3Screen.kt
+++ app/src/main/java/com/example/madcamp_proj1/Tab3Screen.kt
@@ -1,16 +1,109 @@
 package com.example.madcamp_proj1
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.madcamp_proj1.data.Datasource
 
+
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Tab3Screen(modifier: Modifier = Modifier) {
-    Column(modifier = modifier) {
-        Tab3ScreenContact()
-        Tab3ScreenGallery()
+
+    val images = remember { Datasource().loadGalleryImages() }
+    var query by remember { mutableStateOf("") }
+    var queryList: MutableList<String> by remember { mutableStateOf(mutableStateListOf()) }
+
+    Box(modifier = modifier.fillMaxSize()) {
+        Column(modifier = Modifier.fillMaxSize()) {
+
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp)
+                    .background(Color.LightGray, shape = RoundedCornerShape(30))
+            ) {
+                OutlinedTextField(
+                    value = query,
+                    onValueChange = { query = it },
+                    placeholder = {
+                        Row {
+                            Icon(
+                                Icons.Default.Search,
+                                contentDescription = "Search Icon",
+                                tint = Color.Gray
+                            )
+                            Text(" 검색", color = Color.Gray)
+                        }
+                    },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 1.dp, bottom = 1.dp, start = 4.dp, end = 4.dp),
+                    shape = RoundedCornerShape(30),
+                    colors = TextFieldDefaults.outlinedTextFieldColors(
+                        focusedBorderColor = Color.LightGray,
+                        unfocusedBorderColor = Color.LightGray
+                    ),
+                    singleLine = true
+                )
+            }
+            Divider(color = Color.LightGray, thickness = 1.dp)
+
+            val filteredGalleryImageList = images.filter { it ->
+                val sortedNameList = it.nameList.sorted()
+                val sortedQuery = query.split(",").map { it.trim() }.sorted()
+                sortedQuery.all { name -> sortedNameList.contains(name)
+                } || it.time.contains(query.trim())
+            }
+            if(filteredGalleryImageList.size == Datasource().loadGalleryImages().size){
+                LazyColumn(modifier = Modifier.weight(1f)) {
+                    items(queryList) {
+                        Text(
+                            text = it,
+                            modifier = Modifier
+                                .padding(horizontal = 16.dp, vertical = 8.dp)
+                                .fillMaxWidth()
+                        )
+                    }
+                }
+            }
+            else{
+                if(filteredGalleryImageList.isNotEmpty()){
+                    if(queryList.contains(query)) queryList.remove(query)
+                    queryList.add(0, query)
+                }
+
+                GalleryImageGrid(
+                    galleryImageList = filteredGalleryImageList,
+                    modifier = modifier
+                )
+            }
+        }
     }
 }
 
