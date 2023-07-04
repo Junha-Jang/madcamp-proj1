9b38c13536f7b6da30ad3c606f1d8d497506ac2a
Junha Jang <andy.jang@kaist.ac.kr>
2023-07-04 22:00:08 +0900

    feat(tab3): update data class

--- app/src/main/java/com/example/madcamp_proj1/data/Datasource.kt
+++ app/src/main/java/com/example/madcamp_proj1/data/Datasource.kt
@@ -2,70 +2,239 @@ package com.example.madcamp_proj1.data
 
 import com.example.madcamp_proj1.R
 import com.example.madcamp_proj1.model.GalleryImage
-
 class Datasource {
     fun loadGalleryImages(): List<GalleryImage> {
         return listOf(
-            GalleryImage(R.drawable.image1),
-            GalleryImage(R.drawable.image2),
-            GalleryImage(R.drawable.image3),
-            GalleryImage(R.drawable.image4),
-            GalleryImage(R.drawable.image5),
-            GalleryImage(R.drawable.image6),
-            GalleryImage(R.drawable.image7),
-            GalleryImage(R.drawable.image8),
-            GalleryImage(R.drawable.image9),
-            GalleryImage(R.drawable.image10),
-            GalleryImage(R.drawable.image1),
-            GalleryImage(R.drawable.image2),
-            GalleryImage(R.drawable.image3),
-            GalleryImage(R.drawable.image4),
-            GalleryImage(R.drawable.image5),
-            GalleryImage(R.drawable.image6),
-            GalleryImage(R.drawable.image7),
-            GalleryImage(R.drawable.image8),
-            GalleryImage(R.drawable.image9),
-            GalleryImage(R.drawable.image10),
-            GalleryImage(R.drawable.image1),
-            GalleryImage(R.drawable.image2),
-            GalleryImage(R.drawable.image3),
-            GalleryImage(R.drawable.image4),
-            GalleryImage(R.drawable.image5),
-            GalleryImage(R.drawable.image6),
-            GalleryImage(R.drawable.image7),
-            GalleryImage(R.drawable.image8),
-            GalleryImage(R.drawable.image9),
-            GalleryImage(R.drawable.image10),
-            GalleryImage(R.drawable.image1),
-            GalleryImage(R.drawable.image2),
-            GalleryImage(R.drawable.image3),
-            GalleryImage(R.drawable.image4),
-            GalleryImage(R.drawable.image5),
-            GalleryImage(R.drawable.image6),
-            GalleryImage(R.drawable.image7),
-            GalleryImage(R.drawable.image8),
-            GalleryImage(R.drawable.image9),
-            GalleryImage(R.drawable.image10),
-            GalleryImage(R.drawable.image1),
-            GalleryImage(R.drawable.image2),
-            GalleryImage(R.drawable.image3),
-            GalleryImage(R.drawable.image4),
-            GalleryImage(R.drawable.image5),
-            GalleryImage(R.drawable.image6),
-            GalleryImage(R.drawable.image7),
-            GalleryImage(R.drawable.image8),
-            GalleryImage(R.drawable.image9),
-            GalleryImage(R.drawable.image10),
-            GalleryImage(R.drawable.image1),
-            GalleryImage(R.drawable.image2),
-            GalleryImage(R.drawable.image3),
-            GalleryImage(R.drawable.image4),
-            GalleryImage(R.drawable.image5),
-            GalleryImage(R.drawable.image6),
-            GalleryImage(R.drawable.image7),
-            GalleryImage(R.drawable.image8),
-            GalleryImage(R.drawable.image9),
-            GalleryImage(R.drawable.image10)
+            GalleryImage(
+                imageResourceId = R.drawable.image1,
+                nameList = listOf("홍길동", "전지민", "유재석", "김채원", "박명수", "카리나"),
+                time = "2001/01/01"
+            ),
+            GalleryImage(
+                imageResourceId = R.drawable.image2,
+                nameList = listOf("박지성", "전지민", "팽수", "박명수", "윈터", "장병규"),
+                time = "2002/02/02"
+            ),
+            GalleryImage(
+                imageResourceId = R.drawable.image3,
+                nameList = listOf("홍길동", "안정환", "전지민", "김채원", "해밍턴", "박명수"),
+                time = "2003/03/03"
+            ),
+            GalleryImage(
+                imageResourceId = R.drawable.image4,
+                nameList = listOf("유관순", "안정환", "유재석", "이영지", "차은우", "장병규"),
+                time = "2004/04/04"
+            ),
+            GalleryImage(
+                imageResourceId = R.drawable.image5,
+                nameList = listOf("홍길동", "유재석", "이영지", "김채원", "박명수", "윈터"),
+                time = "2005/05/05"
+            ),
+            GalleryImage(
+                imageResourceId = R.drawable.image6,
+                nameList = listOf("홍길동", "안중근", "박지성", "이영지", "차은우", "이강인"),
+                time = "2006/06/06"
+            ),
+            GalleryImage(
+                imageResourceId = R.drawable.image7,
+                nameList = listOf("홍길동", "장원영", "차은우", "라우브", "포스트 말론", "종강"),
+                time = "2007/07/07"
+            ),
+            GalleryImage(
+                imageResourceId = R.drawable.image8,
+                nameList = listOf("이순신", "유재석", "이영지", "박명수", "카리나", "새우탕"),
+                time = "2008/08/08"
+            ),
+            GalleryImage(
+                imageResourceId = R.drawable.image9,
+                nameList = listOf("팽수", "유재석", "장원영", "김채원", "새우탕", "장병규"),
+                time = "2009/09/09"
+            ),
+            GalleryImage(
+                imageResourceId = R.drawable.image10,
+                nameList = listOf("팽수", "김채원", "이강인", "라우브", "카리나", "장병규"),
+                time = "2010/10/10"
+            ),
+            GalleryImage(
+                imageResourceId = R.drawable.image1,
+                nameList = listOf("홍길동", "전지민", "유재석", "김채원", "박명수", "카리나"),
+                time = "2001/01/01"
+            ),
+            GalleryImage(
+                imageResourceId = R.drawable.image2,
+                nameList = listOf("박지성", "전지민", "팽수", "박명수", "윈터", "장병규"),
+                time = "2002/02/02"
+            ),
+            GalleryImage(
+                imageResourceId = R.drawable.image3,
+                nameList = listOf("홍길동", "안정환", "전지민", "김채원", "해밍턴", "박명수"),
+                time = "2003/03/03"
+            ),
+            GalleryImage(
+                imageResourceId = R.drawable.image4,
+                nameList = listOf("유관순", "안정환", "유재석", "이영지", "차은우", "장병규"),
+                time = "2004/04/04"
+            ),
+            GalleryImage(
+                imageResourceId = R.drawable.image5,
+                nameList = listOf("홍길동", "유재석", "이영지", "김채원", "박명수", "윈터"),
+                time = "2005/05/05"
+            ),
+            GalleryImage(
+                imageResourceId = R.drawable.image6,
+                nameList = listOf("홍길동", "안중근", "박지성", "이영지", "차은우", "이강인"),
+                time = "2006/06/06"
+            ),
+            GalleryImage(
+                imageResourceId = R.drawable.image7,
+                nameList = listOf("홍길동", "장원영", "차은우", "라우브", "포스트 말론", "종강"),
+                time = "2007/07/07"
+            ),
+            GalleryImage(
+                imageResourceId = R.drawable.image8,
+                nameList = listOf("이순신", "유재석", "이영지", "박명수", "카리나", "새우탕"),
+                time = "2008/08/08"
+            ),
+            GalleryImage(
+                imageResourceId = R.drawable.image9,
+                nameList = listOf("팽수", "유재석", "장원영", "김채원", "새우탕", "장병규"),
+                time = "2009/09/09"
+            ),
+            GalleryImage(
+                imageResourceId = R.drawable.image10,
+                nameList = listOf("팽수", "김채원", "이강인", "라우브", "카리나", "장병규"),
+                time = "2010/10/10"
+            ),
+            GalleryImage(
+                imageResourceId = R.drawable.image1,
+                nameList = listOf("홍길동", "전지민", "유재석", "김채원", "박명수", "카리나"),
+                time = "2001/01/01"
+            ),
+            GalleryImage(
+                imageResourceId = R.drawable.image2,
+                nameList = listOf("박지성", "전지민", "팽수", "박명수", "윈터", "장병규"),
+                time = "2002/02/02"
+            ),
+            GalleryImage(
+                imageResourceId = R.drawable.image3,
+                nameList = listOf("홍길동", "안정환", "전지민", "김채원", "해밍턴", "박명수"),
+                time = "2003/03/03"
+            ),
+            GalleryImage(
+                imageResourceId = R.drawable.image4,
+                nameList = listOf("유관순", "안정환", "유재석", "이영지", "차은우", "장병규"),
+                time = "2004/04/04"
+            ),
+            GalleryImage(
+                imageResourceId = R.drawable.image5,
+                nameList = listOf("홍길동", "유재석", "이영지", "김채원", "박명수", "윈터"),
+                time = "2005/05/05"
+            ),
+            GalleryImage(
+                imageResourceId = R.drawable.image6,
+                nameList = listOf("홍길동", "안중근", "박지성", "이영지", "차은우", "이강인"),
+                time = "2006/06/06"
+            ),
+            GalleryImage(
+                imageResourceId = R.drawable.image7,
+                nameList = listOf("홍길동", "장원영", "차은우", "라우브", "포스트 말론", "종강"),
+                time = "2007/07/07"
+            ),
+            GalleryImage(
+                imageResourceId = R.drawable.image8,
+                nameList = listOf("이순신", "유재석", "이영지", "박명수", "카리나", "새우탕"),
+                time = "2008/08/08"
+            ),
+            GalleryImage(
+                imageResourceId = R.drawable.image9,
+                nameList = listOf("팽수", "유재석", "장원영", "김채원", "새우탕", "장병규"),
+                time = "2009/09/09"
+            ),
+            GalleryImage(
+                imageResourceId = R.drawable.image10,
+                nameList = listOf("팽수", "김채원", "이강인", "라우브", "카리나", "장병규"),
+                time = "2010/10/10"
+            ),
+            GalleryImage(
+                imageResourceId = R.drawable.image1,
+                nameList = listOf("홍길동", "전지민", "유재석", "김채원", "박명수", "카리나"),
+                time = "2001/01/01"
+            ),
+            GalleryImage(
+                imageResourceId = R.drawable.image2,
+                nameList = listOf("박지성", "전지민", "팽수", "박명수", "윈터", "장병규"),
+                time = "2002/02/02"
+            ),
+            GalleryImage(
+                imageResourceId = R.drawable.image3,
+                nameList = listOf("홍길동", "안정환", "전지민", "김채원", "해밍턴", "박명수"),
+                time = "2003/03/03"
+            ),
+            GalleryImage(
+                imageResourceId = R.drawable.image4,
+                nameList = listOf("유관순", "안정환", "유재석", "이영지", "차은우", "장병규"),
+                time = "2004/04/04"
+            ),
+            GalleryImage(
+                imageResourceId = R.drawable.image5,
+                nameList = listOf("홍길동", "유재석", "이영지", "김채원", "박명수", "윈터"),
+                time = "2005/05/05"
+            ),
+            GalleryImage(
+                imageResourceId = R.drawable.image6,
+                nameList = listOf("홍길동", "안중근", "박지성", "이영지", "차은우", "이강인"),
+                time = "2006/06/06"
+            ),
+            GalleryImage(
+                imageResourceId = R.drawable.image7,
+                nameList = listOf("홍길동", "장원영", "차은우", "라우브", "포스트 말론", "종강"),
+                time = "2007/07/07"
+            ),
+            GalleryImage(
+                imageResourceId = R.drawable.image8,
+                nameList = listOf("이순신", "유재석", "이영지", "박명수", "카리나", "새우탕"),
+                time = "2008/08/08"
+            ),
+            GalleryImage(
+                imageResourceId = R.drawable.image9,
+                nameList = listOf("팽수", "유재석", "장원영", "김채원", "새우탕", "장병규"),
+                time = "2009/09/09"
+            ),
+            GalleryImage(
+                imageResourceId = R.drawable.image10,
+                nameList = listOf("팽수", "김채원", "이강인", "라우브", "카리나", "장병규"),
+                time = "2010/10/10"
+            ),
+            GalleryImage(
+                imageResourceId = R.drawable.image1,
+                nameList = listOf("홍길동", "전지민", "유재석", "김채원", "박명수", "카리나"),
+                time = "2001/01/01"
+            ),
+            GalleryImage(
+                imageResourceId = R.drawable.image2,
+                nameList = listOf("박지성", "전지민", "팽수", "박명수", "윈터", "장병규"),
+                time = "2002/02/02"
+            ),
+            GalleryImage(
+                imageResourceId = R.drawable.image3,
+                nameList = listOf("홍길동", "안정환", "전지민", "김채원", "해밍턴", "박명수"),
+                time = "2003/03/03"
+            ),
+            GalleryImage(
+                imageResourceId = R.drawable.image4,
+                nameList = listOf("유관순", "안정환", "유재석", "이영지", "차은우", "장병규"),
+                time = "2004/04/04"
+            ),
+            GalleryImage(
+                imageResourceId = R.drawable.image5,
+                nameList = listOf("홍길동", "유재석", "이영지", "김채원", "박명수", "윈터"),
+                time = "2005/05/05"
+            ),
+            GalleryImage(
+                imageResourceId = R.drawable.image6,
+                nameList = listOf("홍길동", "안중근", "박지성", "이영지", "차은우", "이강인"),
+                time = "2006/06/06"
+            )
         )
     }
 }
