1258263a762bee25ee4d77aa2b2d477df62865ef
Junha Jang <andy.jang@kaist.ac.kr>
2023-07-04 19:32:55 +0900

    feat(tab3): initial feature/tab3 commit

--- app/src/main/java/com/example/madcamp_proj1/Tab3Screen.kt
+++ app/src/main/java/com/example/madcamp_proj1/Tab3Screen.kt
@@ -1,16 +1,17 @@
 package com.example.madcamp_proj1
 
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
 fun Tab3Screen(modifier: Modifier = Modifier) {
-    Text(
-        text = "This is Tab3",
-        modifier = modifier
-    )
+    Column(modifier = modifier) {
+        Tab3ScreenContact()
+        Tab3ScreenGallery()
+    }
 }
 
 @Preview(showBackground = true)
